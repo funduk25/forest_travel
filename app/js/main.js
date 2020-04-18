6582557adaba45d0474e7ca0d86d69de7756e011
funduk25 <slender330@mail.ru>
2020-04-18 18:51:23 +0800

    added section content

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-$(function(){
-
-
-
-  
+$(function() {
+    $('.arrow-down').click(function() {
+        $('html, body').animate({ scrollTop: $('.content').offset().top }, 'slow');
+        return false;
+    });
 });
