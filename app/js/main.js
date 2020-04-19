705fc59660f2335071e651a9770ba4b70c5b36de
funduk25 <slender330@mail.ru>
2020-04-19 20:27:47 +0800

    create gallery.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
 $(function() {
     $('.arrow-down').click(function() {
-        $('html, body').animate({ scrollTop: $('section.content').offset().top }, 'slow');
+        $('html, body').animate({ scrollTop: $('.content').offset().top }, 'slow');
         return false;
     });
 });
